1173ed5df29149bb7c569b078866fef5b0ec5563
Ben Landon <landb0x207@gmail.com>
2026-05-28 14:27:35 -0700

    fix: restore empty fastfetch patch file

--- packages/fastfetch/default.nix
+++ packages/fastfetch/default.nix
@@ -1,8 +1,10 @@
 { pkgs, ... }:
-pkgs.fastfetch.overrideAttrs {
-  patches = [ ./battery_charge_max.patch ];
+pkgs.fastfetch.overrideAttrs (oldAttrs:
+  assert oldAttrs.version == "2.63.1";
+  {
+  patches = (oldAttrs.patches or []) ++ [ ./battery_charge_max.patch ];
 
   checkPhase = ''
   ctest
   '';
-}
+})
