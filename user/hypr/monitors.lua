b0b8f49480bc9c76b5acb43cc81b70176741be7f
Ben Landon <landb0x207@gmail.com>
2026-06-16 11:27:07 -0700

    feat(hyprland): specify physical positions for monitors

--- user/hypr/monitors.lua
+++ user/hypr/monitors.lua
@@ -1,6 +1,18 @@
 hl.monitor({
-    output = "",
+    output = "eDP-1",
     mode = "preferred",
-    position = "auto",
+    position = "0x1200", -- Place below
+    scale = "1",
+})
+hl.monitor({
+    output = "HDMI-A-1",
+    mode = "preferred",
+    position = "-1920x0", -- Place left
+    scale = "1",
+})
+hl.monitor({
+    output = "DP-1",
+    mode = "preferred",
+    position = "0x0", -- Primary
     scale = "1",
 })
