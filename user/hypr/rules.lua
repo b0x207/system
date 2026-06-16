b1b3e78704b80be2ebdc0bda6619cc9c48b39ce3
Ben Landon <landb0x207@gmail.com>
2026-06-16 10:12:13 -0700

    fix: select DP monitors for desktop when eDP doesn't exist

--- user/hypr/rules.lua
+++ user/hypr/rules.lua
@@ -1,6 +1,26 @@
 -- Default the primary workspaces to any external monitor
+local function primary_monitor_select()
+    -- It is safe to assume that there will always be at least one monitor
+    local selected_monitor = hl.get_monitors()[0]
+
+    for _, monitor in ipairs(hl.get_monitors()) do
+	-- Wherever possible, prefer the full DP monitor but if it can't be
+	-- found, then HDMI-A-1 is a fair substitute
+	if monitor.name == "HDMI-A-1"
+	   and selected_monitor
+	   and selected_monitor.name ~= "DP-1" then
+	    selected_monitor = monitor
+	elseif monitor.name == "DP-1" then
+	    selected_monitor = monitor
+	end
+    end
+
+    return selected_monitor
+end
+
+local primary_monitor = primary_monitor_select()
 for i=1,8 do
-    hl.workspace_rule({ workspace = i, monitor = "HDMI-A-1" })
+    hl.workspace_rule({ workspace = i, monitor = primary_monitor.name })
 end
 
 
